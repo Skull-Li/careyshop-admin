e40487d4833d8498f40ad11a89f8f73f96020fe7
思维空间 <dnyz520@126.com>
2021-01-20 01:57:19 +0800

    package update

--- dependencies-cdn.js
+++ dependencies-cdn.js
@@ -56,7 +56,7 @@ if (process.env.VUE_APP_CDN === 'true') {
     {
       name: 'hotkeys-js',
       library: 'hotkeys',
-      js: 'https://cdn.jsdelivr.net/npm/hotkeys-js@3.8.1/dist/hotkeys.min.js',
+      js: 'https://cdn.jsdelivr.net/npm/hotkeys-js@3.8.2/dist/hotkeys.min.js',
       css: ''
     },
     {
