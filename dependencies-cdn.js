6e586de55ce2eb59da091f4b05b9b56c5e9ae46c
思维空间 <dnyz520@126.com>
2021-03-26 01:34:07 +0800

    package update

--- dependencies-cdn.js
+++ dependencies-cdn.js
@@ -104,7 +104,7 @@ if (process.env.VUE_APP_CDN === 'true') {
     {
       name: 'ua-parser-js',
       library: 'UAParser',
-      js: 'https://cdn.jsdelivr.net/npm/ua-parser-js@0.7.24/dist/ua-parser.min.js',
+      js: 'https://cdn.jsdelivr.net/npm/ua-parser-js@0.7.25/dist/ua-parser.min.js',
       css: ''
     },
     {
