69543b57bf7153d02c6c290dd5c5d3c9405411b7
思维空间 <dnyz520@126.com>
2021-05-25 15:51:47 +0800

    渠道模块初始化

---
+++ src/views/place/index/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <cs-container :is-back="false">
+    <page-navi class="cs-m" v-model="root"/>
+  </cs-container>
+</template>
+
+<script>
+export default {
+  name: 'place-index',
+  components: {
+    PageNavi: () => import('@/layout/header-aside/components/header-navi')
+  },
+  data() {
+    return {
+      root: '/place'
+    }
+  }
+}
+</script>
